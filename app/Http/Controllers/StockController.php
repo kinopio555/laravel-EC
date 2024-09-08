49ea963720310c9246a41be3d4b4d336ce560503
kinopio555 <kinopio582@gmail.com>
2024-09-08 19:35:51 +0900

    五回目の記事分終了

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth; //追加
 use App\Models\Stock; //追加
 use Illuminate\Http\Request;
 use App\Models\UserStock; //追加
@@ -16,10 +17,22 @@ class StockController extends Controller
        return view('stocks',compact('stocks')); //追記変更
    }
    
-   public function myCart()
+   public function myCart(UserStock $userStock)
    {
-       $myCartStocks = UserStock::all();
+       $myCartStocks = $userStock->showMyCart();
        return view('myCart',compact('myCartStocks'));
-       
    }
+
+   public function addMycart(Request $request,UserStock $userStock)
+   {
+
+       //カートに追加の処理
+       $stockId=$request->stockId;
+       $message = $userStock->addCart($stockId);
+
+       //追加後の情報を取得
+       $myCartStocks = $userStock->showMyCart();
+
+       return view('myCart',compact('myCartStocks' , 'message'));
+    }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,9 +11,14 @@
                 </div>
 
                 <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('stock.index')" :active="request()->routeIs('stock.index')">
+                        商品一覧
+                    </x-nav-link>
+                </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('stock.myCart')" :active="request()->routeIs('stock.myCart')">
+                        MyCart
                     </x-nav-link>
                 </div>
             </div>
